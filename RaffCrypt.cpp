761050cfaf79d1f3d389711981510f7a2e9aebcd
Raff <86250367+Rafffyy@users.noreply.github.com>
2021-06-21 23:04:44 +0200

    Update RaffCrypt.cpp

--- RaffCrypt.cpp
+++ RaffCrypt.cpp
@@ -1,1 +1,13 @@
+#include "RaffCrypt.h"
+#include <iostream>
 
+int main()
+{
+    auto testString = raffCrypt("Raff Is nice for making this"); // encrypted at compile-time
+    printf(testString);	// automatic decryption on usage (alternatively .decrypt())
+    testString.encrypt(); // encrypt after usage if needed again
+    testString.clear();	// set full string storage to 0
+
+    getchar();
+    return 0x0;
+}
